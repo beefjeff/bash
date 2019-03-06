665a5ee54c3eb31e300f3e2b6a7178d620a2d227
Ethan Brace <ethanabrace@gmail.com>
2019-03-06 04:39:57 -0500

    add old bash files on pc1

--- functions.sh
+++ functions.sh
@@ -3,10 +3,99 @@
 
 
 FILE_ALIASES=~/bash/aliases.sh
-PRIVATE_ALIASES=~/bash/private-alias.sh
 
-test(){
-		echo 'test'
+
+drw(){
+
+	cd /mnt/c/users/ethan/Code/drwindows;
+	restart_horizon="r"
+	restart_server="r"
+	restart_watcher="r"
+
+	sudo /etc/init.d/screen-cleanup start
+
+	if screen -list | grep -q "horizon"; then
+
+		prompt "Horizon already running. [r]estart [k]ill [enter]ignore" restart_horizon
+
+		if [[ $restart_horizon == "r" ]] || [[ $restart_horizon == "k" ]]; then
+			echo "Killing Horizon"
+			screen -S horizon -X quit
+		fi
+
+
+	fi	
+
+	if screen -list | grep -q "server"; then
+
+		prompt "Server already running. [r]estart [k]ill [enter]ignore" restart_server
+
+		if [[ $restart_server == "r" ]] || [[ $restart_server == "k" ]]; then
+			echo "Killing Active Server"
+			screen -S server -X quit
+		fi
+
+
+	fi	
+
+	if screen -list | grep -q "watcher"; then
+
+		prompt "Watcher already running. [r]estart [k]ill [enter]ignore" restart_watcher
+
+		if [[ $restart_watcher == "r" ]] || [[ $restart_watcher == "k" ]]; then
+
+			echo "Killing Active Watcher"
+			screen -S watcher -X quit
+
+		fi
+
+	fi	
+
+
+
+
+	# restart the screens if we need to
+	if ! screen -list | grep -q "horizon"; then
+
+		if [[ $restart_horizon != "k" ]]; then
+
+			echo "Starting Horizon"
+			screen -dmS horizon
+			screen -S horizon -X stuff 'cd /mnt/c/users/ethan/Code/drwindows; php artisan horizon\n'
+
+		fi
+
+	fi	
+
+	if ! screen -list | grep -q "server"; then
+
+		if [[ $restart_server != "k" ]]; then
+			echo "Starting Server"
+			screen -dmS server
+			screen -S server -X stuff 'cd /mnt/c/users/ethan/Code/drwindows; serve\n'
+		fi
+
+	fi	
+
+	if ! screen -list | grep -q "watcher"; then
+
+		if [[ $restart_watcher != "k" ]]; then
+			echo "Starting Watcher"
+			screen -dmS watcher
+			screen -S watcher  -X stuff 'cd /mnt/c/users/ethan/Code/drwindows; npm run watch\n'
+		fi
+
+	fi	
+
+
+}
+
+nova(){
+	php artisan nova:$@
+}
+
+horizon(){
+	php artisan horizon:$@
 }
 
 say() {
@@ -167,24 +256,8 @@ add-alias(){
 
 	prompt 'Enter the command:' ALIAS_BODY
 
-	prompt 'Is this private? [y/n] (wont be tracked)' PRIVATE_ALIAS
-
-	if echo "$PRIVATE_ALIAS" | grep  -iq "^y" ;then
-		echo "adding to $PRIVATE_ALIASES"
-		echo "alias $ALIAS_NAME='$ALIAS_BODY'" >> $PRIVATE_ALIASES;
-
-
-	else
-		echo "adding to $FILE_ALIASES"
-		echo "alias $ALIAS_NAME='$ALIAS_BODY'" >> $FILE_ALIASES;
-	fi
-
-
-
-
-	echo "Alias added. Sourcing files..."
+	echo "alias $ALIAS_NAME='$ALIAS_BODY'" >> $FILE_ALIASES;
 	reload
-	echo "Done."
 }
 
 
@@ -265,8 +338,10 @@ reload(){
 
 
 
-prompt(){ # prompt-text variable-name
+prompt(){ 
+	# prompt-text variable-name
 	# check bash type
+
 	in-zsh(){
 		out=$(ps  -ef | grep $$ | grep -v grep);
 		if [[ $out == *"zsh"* ]]; then
