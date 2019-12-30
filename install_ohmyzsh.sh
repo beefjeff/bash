5c7db4bac40181359ed51ed7cb5af42ab9f1689b
Ethan Brace <ethanabrace@gmail.com>
2019-12-30 10:57:04 -0500

    add failure note

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -1,1 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# if this fails due to invalid flag, install newer version of git 
+# centos ->  
+#	yum install http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm && yum install git
+
