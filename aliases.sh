a1adefe8c57090d82407bf12251ee72fa2768dca
Ethan Brace <ethanabrace@gmail.com>
2020-03-24 14:12:08 -0400

    add git update script

--- aliases.sh
+++ aliases.sh
@@ -3,8 +3,6 @@
 ### common aliases ###
 
 alias s='sudo'
-alias n='nano'
-alias sn='sudo nano'
 alias ls='clear; ll -a --color --group-directories-first'
 alias cl='clear'
 alias aa='add-alias'
@@ -34,37 +32,29 @@ alias pstorm-help='composer require --dev barryvdh/laravel-ide-helper;composer r
 
 ### added from terminal with add-alias() ###
 alias python='python3'
-alias d-ethan.braceyourself='cd /mnt/c/users/ethan/Code/ethan.braceyourself.solutions'
-alias d-leagueasy='cd ~/Code/leagueasy.braceyourself.solutions'
-alias d-drw='cd /mnt/c/users/ethan/Code/drwindows'
 alias sv='sudo vim'
 alias sag='sudo apt-get'
 alias aliases='n ~/.bash_aliases'
 alias clear-vim-swap='rm ~/.vim/swap/**'
-alias art='php artisan'
 alias lara-remigrate='php artisan migrate:refresh && php artisan db:seed'
 alias q='exit'
 alias lara-keygen='php artisan key:generate'
 alias ping='ping -c 3'
 alias suweb='sudo -u www-data'
-alias watch='npm run watch'
 alias mb-info='sudo dmidecode -t 2'
 alias copy-envoy='cp ~/Envoy.blade.php ./'
 alias envoy='~/.composer/vendor/bin/envoy'
-alias deploy='git push; ~/.composer/vendor/bin/envoy run deploy'
 alias push='git push; ~/.composer/vendor/bin/envoy run push'
 alias lara-push-to-production='npm run production; git push; envoy run deploy; npm run dev;'
 alias lara-log-watch='tail -f storage/logs/laravel.log'
-alias d-windows.braceyourself='cd /mnt/c/users/ethan/Code/windows.braceyourself.solutions'
 alias extip='echo $EXTIP'
 alias cmd='/mnt/c/Windows/System32/cmd.exe /c'
 alias psh='/mnt/c/Windows/System32/cmd.exe /c powershell -command "$@"'
 alias v-component='php artisan vueg:component'
 alias npm='/usr/bin/npm'
 alias serve='php artisan serve'
-
+alias art='php artisan'
 alias screens='screen -ls'
-
 alias g--='git checkout --'
 
 
@@ -82,3 +72,4 @@ alias dfimage='docker run -v /var/run/docker.sock:/var/run/docker.sock --rm chen
 alias dockerup='docker-compose up -d'
 alias dockerdown='docker-compose down'
 alias dc='docker-compose'
+alias gamend='git commit --amend'
